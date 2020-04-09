82b8a17c663a605c76cd581a75a1fda0ca886665
Winnie Lam <winnielam@Winnies-MacBook-Air.local>
2020-04-09 12:51:56 -0400

    Updates

---
+++ precache-manifest.8cd432d8699fdc8593c0d909bcacc505.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06cec7b032155b61f9be4328f64b2948",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "37925a13ffaa1924d074",
+    "url": "/tic-tac-toe/static/css/main.aca99ef4.chunk.css"
+  },
+  {
+    "revision": "6c84d17184dc19a8d6af",
+    "url": "/tic-tac-toe/static/js/2.fe3becc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.fe3becc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37925a13ffaa1924d074",
+    "url": "/tic-tac-toe/static/js/main.79346e99.chunk.js"
+  },
+  {
+    "revision": "28dfba9420967d301740",
+    "url": "/tic-tac-toe/static/js/runtime-main.4c12a8d1.js"
+  }
+]);
